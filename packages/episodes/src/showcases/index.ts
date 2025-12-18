ebd5b5505bde0ff03c566a5fad97f785e00b8de1
Nishit Gupta <nishit.gupta6708@gmail.com>
2025-12-18 16:10:34 +0530

    Updates: for package

---
+++ packages/episodes/src/showcases/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Showcases - Demo episodes for feature demonstration
+ *
+ * Import this file in Root.tsx to auto-register all showcase episodes:
+ *   import "@tokovo/episodes/src/showcases";
+ *
+ * To add a new showcase:
+ *   1. Create: showcases/my-showcase.episode.ts
+ *   2. Add import below
+ */
+
+// Add showcase imports here as you create them:
+// import "./camera-demo.episode";
+// import "./audio-demo.episode";
---
+++ packages/episodes/src/tests/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Tests - Test episodes for quality assurance
+ *
+ * Import this file in Root.tsx to auto-register all test episodes:
+ *   import "@tokovo/episodes/src/tests";
+ *
+ * To add a new test episode:
+ *   1. Create: tests/my-test.episode.ts
+ *   2. Add import below
+ */
+
+// Add test imports here as you create them:
+// import "./keyboard-test.episode";
+// import "./camera-test.episode";
