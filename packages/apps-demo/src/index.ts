02ce1b6b54b5af46732f62a742ee7927d71a8dd8
Nishit Gupta <nishit.gupta6708@gmail.com>
2026-01-28 03:18:56 +0530

    idk

--- packages/apps-demo/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Barrel exports for Demo Notes plugin
-export { DemoPlugin, registerDemoPlugin } from "./plugin";
-export type { DemoState, Note } from "./runtime/state";
-export { createDemoInitialState } from "./runtime/state";
-export { demoReducer } from "./runtime/reducer";
-export * from "./runtime/selectors";
