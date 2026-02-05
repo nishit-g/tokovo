085eaa7da5514df2ffd0946d78f4c443f62ab9df
Nishit Gupta <nishit.gupta6708@gmail.com>
2026-02-06 04:01:07 +0530

    remoooove

--- apps/web/src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx'
-
-export function Logo({ className }: { className?: string }) {
-  return (
-    <span className={clsx('font-medium tracking-tight text-white', className)}>
-      tokovo<span className="text-gray-500">.</span><span className="text-gray-600">studio</span>
-    </span>
-  )
-}
